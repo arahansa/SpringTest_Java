ff7da6e2cc99c6ece4d089d232a6db28c0f923fe
KangYong <arahansa@naver.com>
2015-06-16 09:52:47 +0900

    lambda applied for sum from 1 to 100

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,5 +1,9 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class Calculator {
 
 	public static void main(String[] args) {
@@ -8,5 +12,11 @@ public class Calculator {
 			sum+=i;
 		}
 		System.out.println("1부터 100까지의 합 : " +sum);
+		
+		sum = IntStream.range(1, 101).sum();
+		System.out.println("1부터 100까지의 합 : " +sum);
 	}
+	
+	
+	
 }
