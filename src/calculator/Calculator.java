fd6832782a49ad389a1f30bce55d861600072e7c
KangYong <arahansa@naver.com>
2015-06-16 11:53:37 +0900

    add - sum with list

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -6,28 +6,61 @@ import java.util.stream.IntStream;
 
 public class Calculator {
 	
+	
+	
 	public static void main(String[] args) {
+		Calculator calculator = new Calculator();
 		int sum = 0;
+		
 		//for문
-		for (int i = 1; i <= 100; i++) {
+		for (int i = 1; i <= 100; i++) 
 			sum+=i;
-		}
-		System.out.println("1부터 100까지의 합 : " +sum);
+		calculator.printSum("포문 : ", sum);
 		
 		//람다
 		sum = IntStream.range(1, 101).sum();
-		System.out.println("1부터 100까지의 합 : " +sum);
+		calculator.printSum("람다 :", sum);
 		
 		//재귀함수
-		sum = sum(100);
-		System.out.println("1부터 100까지의 합 : " +sum);
+		sum = calculator.sum(100);
+		calculator.printSum("재귀함수 :", sum);
+		
+		// ArrayList
+		sum = calculator.sum(calculator.range(1, 100));
+		calculator.printSum("리스트추가 :", sum);
+	}
+
+	//재귀함수에 쓰인 sum
+	private int sum(int i){
+		return (i==1) ? 1 : sum(i-1)+i;
+	}
+
+	 /*	 IntStream 레인지같이 시도를해볼까?! 전역변수로 list를 뺄까말까 고민을 했었다.
+	 range초기에는 Arraylist선언후 i부터 넣고  
+	 subRange를 돌려 i를 늘려가면서 j까지 더함 */
+	private List<Integer> range(int i, int j){
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(i);
+		return (i==j) ? list : subRange(list, ++i, j);
+	}
+	private List<Integer> subRange(List<Integer> list, int i, int j){
+		list.add(i);
+		return (i==j) ? list : subRange(list, ++i, j);
 	}
 	
-	private static int sum(int i){
-		if (i==1){
-			return 1;
+	// IntStream sum 같이? 체인연결하고싶지만 욕심같다. 파라미터로 연결!ㅠ
+	private int sum(List<Integer> list){
+		return sum(list, 0 , list.size()-1); 	
+	}
+	private int sum(List<Integer> list, int i, int maximum) {
+		if(i==maximum){
+			return list.get(maximum);
 		}else{
-			return sum(i-1)+i;
+			return list.get(i)+sum(list, ++i, maximum);
 		}
+	}
+
+	private  void printSum(String msg , int sum) {
+		System.out.println(msg + "1부터 100까지의 합 : " +sum);
 	}
 }
